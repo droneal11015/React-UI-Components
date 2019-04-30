547693bb81a9748e8d2086efb3239ddedfd35423
droneal11015 <droneal11015@gmail.com>
2019-04-30 17:42:57 -0400

    completed calculator

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = props => {
+    return (
+        <React.Fragment>
+            <div className = 'display'>0</div>
+        </React.Fragment>
+    );
+}
+
+export default CalculatorDisplay;
